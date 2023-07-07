d36d9da182e5c74fa74ee91ff84b14c8777b904a
IamFlowZ <hello@dakotalewallen.me>
2023-07-07 15:12:17 -0400

    improvements for mobile

--- src/App.js
+++ src/App.js
@@ -77,163 +77,248 @@ export default function Page(props) {
 
   return (
     <>
-    <nav style={{
-      display: 'flex',
-      backgroundColor: 'var(--foreground)',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100vw',
-      paddingTop: '0.5rem',
-      paddingBottom: '0.5rem',
-    }}>
-      fediexplorer
-    </nav>
-    <main
-      style={{
-        backgroundColor: "var(--foreground)",
-        padding: "1rem",
-        margin: "2rem",
-        borderRadius: "1rem",
-      }}
-    >
-      <div
+      <nav
         style={{
           display: "flex",
+          backgroundColor: "var(--foreground)",
           justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          paddingTop: "0.5rem",
+          paddingBottom: "0.5rem",
         }}
       >
-        <div>
-          {
-            // feel like i shouldn't have to do this........
-            instanceData.thumbnail && (
-              <img
-                src={instanceData.thumbnail.url}
-                alt="Instance header"
-                max-width="100%"
-                height="auto"
-              />
-            )
-          }
-          <h2>{instanceData.title}</h2>
-          <p>{instanceData.description}</p>
+        fediexplorer
+      </nav>
+      <main
+        style={{
+          backgroundColor: "var(--background)",
+          padding: "1rem",
+          margin: "2rem",
+          borderRadius: "1rem",
+          maxWidth: "100vw",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
           <div
             style={{
-              display: "flex",
-              justifyContent: "space-between",
+              maxWidth: "1300px",
             }}
           >
-            <div>
-              <h2>Rules</h2>
-              <ul>
-                {instanceRuleData &&
-                  instanceRuleData.map((rule) => (
-                    <li key={rule.id}>{rule.text}</li>
+            {
+              // feel like i shouldn't have to do this........
+              instanceData.thumbnail && (
+                <img
+                  src={instanceData.thumbnail.url}
+                  alt="Instance header"
+                  width="100%"
+                  max-width="100%"
+                  height="auto"
+                  style={{
+                    borderRadius: "1rem",
+                  }}
+                />
+              )
+            }
+            <h2>{instanceData.title}</h2>
+            <p>{instanceData.description}</p>
+            <hr
+              style={{
+                marginTop: "1rem",
+                marginBottom: "1rem",
+              }}
+            />
+            <div
+              id="instance-details"
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                style={{
+                  flexBasis: 0,
+                  flexGrow: 1,
+                }}
+              >
+                <h2>Rules</h2>
+                <ul>
+                  {instanceRuleData &&
+                    instanceRuleData.map((rule) => (
+                      <li key={rule.id}>{rule.text}</li>
+                    ))}
+                </ul>
+              </div>
+
+              <div
+                style={{
+                  flexBasis: 0,
+                  flexGrow: 1,
+                  paddingRight: "1rem",
+                }}
+              >
+                <div
+                  className="instance-detail"
+                  style={{
+                    display: "flex",
+                  }}
+                >
+                  <h2>Peers ✅</h2>
+                  {/* <p>{instancePeerData.length}</p> */}
+                  {/* todo: disable next once on last page */}
+                  <p
+                    style={{
+                      alignSelf: "center",
+                      marginLeft: "auto",
+                    }}
+                  >
+                    Page: {peerPage / 10 + 1} of{" "}
+                    {Math.ceil(instancePeerData.length / 10)}
+                  </p>
+                </div>
+                <div
+                  className="page-ctrl-group"
+                  style={{
+                    display: "flex",
+                  }}
+                >
+                  <input
+                    type="text"
+                    placeholder="search"
+                    onChange={(event) => {
+                      const newPeers = instancePeerData.filter((peer) => {
+                        return peer.includes(event.target.value);
+                      });
+                      setShownPeers(newPeers.slice(peerPage, peerPage + 10));
+                    }}
+                  />
+                  <div
+                    className="page-btn-group"
+                    style={{
+                      marginLeft: "auto",
+                    }}
+                  >
+                    <button
+                      style={{
+                        marginRight: "0.25rem",
+                      }}
+                      onClick={() => setPeerPage(peerPage - 10)}
+                      disabled={!peerPage}
+                    >
+                      &lt; Prev
+                    </button>
+                    <button onClick={() => setPeerPage(peerPage + 10)}>
+                      Next &gt;
+                    </button>
+                  </div>
+                </div>
+                <ul>
+                  {shownPeers.map((peer, i) => (
+                    <li
+                      onClick={() => {
+                        const newParams = new URLSearchParams();
+                        newParams.set("instance", peer);
+                        window.location.search = newParams.toString();
+                      }}
+                      key={i}
+                    >
+                      {peer}
+                    </li>
                   ))}
-              </ul>
-            </div>
+                </ul>
+              </div>
 
-            <div>
-              <h2>Peers ✅</h2>
-              {/* <p>{instancePeerData.length}</p> */}
-              {/* todo: disable next once on last page */}
               <div
                 style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "end",
+                  flexBasis: 0,
+                  flexGrow: 1,
                 }}
               >
-                <p>
-                  Page: {peerPage / 10 + 1} of{" "}
-                  {Math.ceil(instancePeerData.length / 10)}
-                </p>
-                <div>
-                  <input type="text" placeholder="search" onChange={(event) => {
-                    const newPeers = instancePeerData.filter((peer) => {
-                      return peer.includes(event.target.value)
-                    })
-                    setShownPeers(newPeers.slice(peerPage, peerPage + 10))
-                  }}/>
-                  <button onClick={() => setPeerPage(peerPage + 10)}>
-                    Next
-                  </button>
-                  <button
-                    onClick={() => setPeerPage(peerPage - 10)}
-                    disabled={!peerPage}
-                  >
-                    Previous
-                  </button>
-                </div>
-              </div>
-              <ul>
-                {shownPeers.map((peer, i) => (
-                  <li
-                    onClick={() => {
-                      const newParams = new URLSearchParams();
-                      newParams.set("instance", peer);
-                      window.location.search = newParams.toString();
+                <div
+                  className="instance-detail"
+                  style={{
+                    display: "flex",
+                  }}
+                >
+                  <h2>Blocked instances ❌</h2>
+                  <p
+                    style={{
+                      alignSelf: "center",
+                      marginLeft: "auto",
                     }}
-                    key={i}
                   >
-                    {peer}
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div>
-              <h2>Blocked instances ❌</h2>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "end",
-                }}
-              >
-                {/* <p>{instanceBlocksData.length}</p> */}
-                {/* todo: disable next once on last page */}
-                <p>
-                  Page: {blockPage / 10 + 1} of{" "}
-                  {Math.ceil(instanceBlocksData.length / 10)}
-                </p>
-                <div>
-                  <input type="text" placeholder="search" onChange={(event) => {
-                    const newBlocks = instanceBlocksData.filter((block) => {
-                      return block.domain.includes(event.target.value)
-                    })
-                    setShownBlocks(newBlocks.slice(blockPage, blockPage + 10))
-                  }}/>
-                  <button onClick={() => setBlockPage(peerPage + 10)}>
-                    Next
-                  </button>
-                  <button
-                    onClick={() => setBlockPage(peerPage - 10)}
-                    disabled={!blockPage}
-                  >
-                    Previous
-                  </button>
+                    Page: {blockPage / 10 + 1} of{" "}
+                    {Math.ceil(instanceBlocksData.length / 10)}
+                  </p>
                 </div>
-              </div>
-              <ul>
-                {shownBlocks.map((block, i) => (
-                  <li
-                    onClick={() => {
-                      const newParams = new URLSearchParams();
-                      newParams.set("instance", block.domain);
-                      window.location.search = newParams.toString();
+                <div
+                  className="page-ctrl-group"
+                  style={{
+                    display: "flex",
+                  }}
+                >
+                  {/* <p>{instanceBlocksData.length}</p> */}
+                  {/* todo: disable next once on last page */}
+                  <input
+                    style={{
+                      marginRight: "auto",
                     }}
-                    key={i}
+                    type="text"
+                    placeholder="search"
+                    onChange={(event) => {
+                      const newBlocks = instanceBlocksData.filter((block) => {
+                        return block.domain.includes(event.target.value);
+                      });
+                      setShownBlocks(
+                        newBlocks.slice(blockPage, blockPage + 10)
+                      );
+                    }}
+                  />
+                  <div
+                    className="page-btn-group"
+                    style={{
+                      marginLeft: "auto",
+                    }}
                   >
-                    {block.domain}: {block.severity}
-                  </li>
-                ))}
-              </ul>
+                    <button
+                      style={{
+                        marginRight: "0.25rem",
+                      }}
+                      onClick={() => setBlockPage(peerPage - 10)}
+                      disabled={!blockPage}
+                    >
+                      &lt; Prev
+                    </button>
+                    <button onClick={() => setBlockPage(peerPage + 10)}>
+                      Next &gt;
+                    </button>
+                  </div>
+                </div>
+                <ul>
+                  {shownBlocks.map((block, i) => (
+                    <li
+                      onClick={() => {
+                        const newParams = new URLSearchParams();
+                        newParams.set("instance", block.domain);
+                        window.location.search = newParams.toString();
+                      }}
+                      key={i}
+                    >
+                      {block.domain}: {block.severity}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div></div>
-    </main>
+        <div></div>
+      </main>
     </>
   );
 }
